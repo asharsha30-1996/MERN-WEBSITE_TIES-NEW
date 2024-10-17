141b1bd4361ace338d89a533739c026d570fc103
Harshavardhana A S <104059964+asharsha30-1996@users.noreply.github.com>
2024-10-17 15:34:22 -0500

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -46,7 +46,7 @@ mongodb.MongoClient.connect(url, { useUnifiedTopology: true })
   }
 
 // Use dynamic port assignment or default to 4000 for local development
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
