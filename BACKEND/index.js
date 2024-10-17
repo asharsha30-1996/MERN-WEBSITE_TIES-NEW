c250539ef55e050c1da9ec70196083cd29678f7f
Harshavardhana A S <104059964+asharsha30-1996@users.noreply.github.com>
2024-10-16 22:05:50 -0500

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -46,7 +46,7 @@ mongodb.MongoClient.connect(url, { useUnifiedTopology: true })
   }
 
 // Start the server
-const PORT =process.env.REACT_APP_API_URL;
+const PORT =process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
